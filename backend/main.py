53def1685e1e09bea3cd4c1cedc79f388c8ad7a3
Paul Clue <67766160+Paul-Clue@users.noreply.github.com>
2024-12-30 17:48:04 -0500

    Update coors middleware and add database production url

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,10 @@ def init_app():
   app.add_middleware(
     CORSMiddleware,
     # allow_origins=["*"],
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+      "http://localhost:3000",
+      "https://llm-evaluator-89mh.vercel.app/",
+      ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
